4ae3b3eac38a695c475b29740d789a742682f0d3
ToniMorey <156945312+ToniMorey@users.noreply.github.com>
2024-11-05 10:28:38 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from streamlit_folium import st_folium
+import folium
+
+# Título de la aplicación
+st.title("Mapa Empreses Campiones")
+
+# Cargar el mapa HTML de Folium
+map_path = "C:/Users/Toni Morey/Desktop/Altres coses/Folium/mapa_empresas.html"  # Asegúrate de que la ruta sea correcta
+with open(map_path, "r", encoding="utf-8") as file:
+    mapa_html = file.read()
+
+# CSS para centrar el mapa
+st.markdown(
+    """
+    <style>
+    .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Mostrar el mapa dentro de un div centrado
+st.markdown('<div class="center">', unsafe_allow_html=True)
+st.components.v1.html(mapa_html, width=1000, height=500, scrolling=True)
+st.markdown('</div>', unsafe_allow_html=True)
